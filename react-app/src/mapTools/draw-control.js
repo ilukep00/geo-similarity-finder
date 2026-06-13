f28a8a134ca0ac83c1c37a2bb0e3ea2052ee0da9
ilukep00 <lukepastor23@gmail.com>
2026-06-13 14:06:13 +0200

    [geo-similarity-finder] Calling to the service from the react app
    * When creating the geom

--- react-app/src/mapTools/draw-control.js
+++ react-app/src/mapTools/draw-control.js
@@ -15,6 +15,28 @@ const DRAW_VECTORTYPES_SETTINGS = {
   circlemarker: false,
 };
 
+async function findSimilarRegions(regionJSON) {
+  const queryBody = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      r_geometry: JSON.stringify(regionJSON),
+    }),
+  };
+
+  const response = await fetch(
+    "http://127.0.0.1:8000/findSimilarRegions/",
+    queryBody,
+  ).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  });
+}
+
 function manageDrawControl(map) {
   var drawnItems = new L.FeatureGroup();
   map.addLayer(drawnItems);
@@ -31,9 +53,12 @@ function manageDrawControl(map) {
   var drawControl = new L.Control.Draw(drawControlOptions);
   map.addControl(drawControl);
 
-  map.on(L.Draw.Event.CREATED, function (e) {
+  map.on(L.Draw.Event.CREATED, async function (e) {
     const { layer } = e;
- 
+    const layerJSON = layer.toGeoJSON();
+
+    await findSimilarRegions(layerJSON);
+
     drawnItems.addLayer(layer);
   });
 }
